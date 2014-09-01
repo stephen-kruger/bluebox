267bb3cd35fe8d75de2deedfd7fc7d190eb1b0c2
stephen.paul.kruger@gmail.com <stephen.paul.kruger@gmail.com@f22217a0-1c79-1dbe-0307-af9e76ebe169>
2014-09-01 15:34:31 +0000

    Change default period to 5 weeks instead of 4

--- bluebox/src/main/java/com/bluebox/smtp/storage/mongodb/StorageImpl.java
+++ bluebox/src/main/java/com/bluebox/smtp/storage/mongodb/StorageImpl.java
@@ -3,7 +3,6 @@ package com.bluebox.smtp.storage.mongodb;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
