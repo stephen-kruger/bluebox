36cf41c9aae8a3f7fda6ecfd729c9f1bb7f9ae5b
stephen.paul.kruger@gmail.com <stephen.paul.kruger@gmail.com@f22217a0-1c79-1dbe-0307-af9e76ebe169>
2014-11-19 15:52:39 +0000

    Implemented smtp blacklist

--- bluebox/src/main/java/com/bluebox/smtp/BlueboxMessageHandlerFactory.java
+++ bluebox/src/main/java/com/bluebox/smtp/BlueboxMessageHandlerFactory.java
@@ -2,16 +2,18 @@ package com.bluebox.smtp;
 
 import java.util.List;
 
-import org.mortbay.log.Log;
 import org.subethamail.smtp.MessageContext;
 import org.subethamail.smtp.MessageHandler;
 import org.subethamail.smtp.MessageHandlerFactory;
 import org.subethamail.smtp.helper.SimpleMessageListenerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.bluebox.Config;
 
 
 public class BlueboxMessageHandlerFactory extends SimpleMessageListenerAdapter implements MessageHandlerFactory {
+	private static final Logger log = LoggerFactory.getLogger(BlueboxMessageHandlerFactory.class);
 
 	private List<String> smtpBlackList;
 
@@ -36,7 +38,7 @@ public class BlueboxMessageHandlerFactory extends SimpleMessageListenerAdapter i
 
 	@Override
 	public MessageHandler create(MessageContext ctx) {
-		Log.info("Message from {} with address {}",ctx.getHelo(),ctx.getRemoteAddress().toString());
+		log.info("Message from {} with address {}",ctx.getHelo(),ctx.getRemoteAddress().toString());
 		if ((isBlackListed(ctx.getHelo())||(isBlackListed(ctx.getRemoteAddress().toString())))) {
 			return null;
 		}
