a690e4cb9f85add5c6be7b6f1faa3ca06a238875
stephen.paul.kruger@gmail.com <stephen.paul.kruger@gmail.com@f22217a0-1c79-1dbe-0307-af9e76ebe169>
2014-12-08 12:06:20 +0000

    Adding rpm build script

--- bluebox/scripts/rpm-build.bash
+++ bluebox/scripts/rpm-build.bash
@@ -1,1 +1,51 @@
-# this script publishes the RPM to the Git hosted yum repository
+#!/bin/bash
+export SCRIPT_DIR=`dirname "$BASH_SOURCE"`
+export BLUEBOX_SRC=$SCRIPT_DIR/..
+
+function command_exists () {
+        if [ -e $1 ]; then
+		return 1
+	else
+		return 0
+	fi
+}
+
+
+function checkMaven() {
+        if command_exists `which mvn`; then
+        echo "Please install Maven:"
+        echo "sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo"
+        echo "sudo yum install apache-maven"
+        exit
+        else
+                echo "Maven detected"
+        fi
+}
+
+function checkSvn() {
+        if command_exists `which svn`; then
+        echo "Please install subversion"
+        exit
+        else
+                echo "Subversion detected"
+        fi
+}
+checkMaven
+checkSvn
+
+cd $BLUEBOX_SRC
+export BLUEBOX_SRC=`pwd`
+
+echo "Updating sources"
+svn update
+rm -rf $BLUEBOX_SRC/target/rpm/*
+
+echo "Building sources"
+mvn clean compile war:war rpm:rpm
+
+echo "Creating local Yum repository in $BLUEBOX_SRC/target/rpm/bluebox/RPMS/noarch"
+createrepo $BLUEBOX_SRC/target/rpm/bluebox/RPMS/noarch
+
+echo "Checking rpm info"
+rpm -qip `find $BLUEBOX_SRC -name bluebox*.rpm`
+sudo yum clean all
