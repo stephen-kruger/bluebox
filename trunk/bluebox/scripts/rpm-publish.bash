8df66e31373eec6974ad06109f0160969d7ee3f0
stephen.paul.kruger@gmail.com <stephen.paul.kruger@gmail.com@f22217a0-1c79-1dbe-0307-af9e76ebe169>
2014-12-09 10:01:21 +0000

    changes

--- trunk/bluebox/scripts/rpm-publish.bash
+++ trunk/bluebox/scripts/rpm-publish.bash
@@ -8,17 +8,17 @@ export BLUEBOX_GIT=$SCRIPT_DIR/../bluebox.git
 cd $BLUEBOX_GIT
 export BLUEBOX_GIT=`pwd`
 
-echo "Copying new repo info to Github root $BLUEBOX_GIT/yum"
+echo "Copying new repo info from $BLUEBOX_SRC to Github root $BLUEBOX_GIT/yum"
 cp -R $BLUEBOX_SRC/target/rpm/bluebox/RPMS/noarch $BLUEBOX_SRC/../bluebox.git/yum
 
 echo "Creating local Yum repository in $BLUEBOX_GIT/noarch"
 createrepo $BLUEBOX_GIT/noarch
 
-echo "Checking rpm info"
-rpm -qip `find $BLUEBOX_GIT/yum -name bluebox*.rpm`
+#echo "Checking rpm info"
+#rpm -qip `find $BLUEBOX_GIT/yum -name bluebox*.rpm`
+
+git add yum
+git commit -m "Updating yum repo with new build"
+git push git@github.com:stephen-kruger/bluebox.git gh-pages
+
 sudo yum clean all
-
-
-#git add yum
-#git commit -m "Updating yum repo with new build"
-#git push git@github.com:stephen-kruger/bluebox.git gh-pages
