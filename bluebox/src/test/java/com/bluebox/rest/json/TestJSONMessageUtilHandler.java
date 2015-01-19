c5c20e8cf50f352911257ac7e69f159d2db81585
stephen.paul.kruger@gmail.com <stephen.paul.kruger@gmail.com@f22217a0-1c79-1dbe-0307-af9e76ebe169>
2015-01-19 18:44:36 +0000

    fixed bug in rest service and unit test

--- bluebox/src/test/java/com/bluebox/rest/json/TestJSONMessageUtilHandler.java
+++ bluebox/src/test/java/com/bluebox/rest/json/TestJSONMessageUtilHandler.java
@@ -9,16 +9,34 @@ import org.codehaus.jettison.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.bluebox.TestUtils;
 import com.bluebox.servlet.BaseServletTest;
 import com.bluebox.smtp.storage.BlueboxMessage;
 import com.bluebox.smtp.storage.LiteMessage;
+import com.bluebox.smtp.storage.StorageFactory;
 
 public class TestJSONMessageUtilHandler extends BaseServletTest {
 	private static final Logger log = LoggerFactory.getLogger(TestJSONMessageUtilHandler.class);
 
 
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		TestUtils.addRandomDirect(StorageFactory.getInstance(), COUNT);
+	}
+
+
+	@Override
+	protected void tearDown() throws Exception {
+		// TODO Auto-generated method stub
+		super.tearDown();
+	}
+
+
 	public void testGetLinks() throws IOException, Exception {
 		List<LiteMessage> list = getInbox().listInboxLite(null, BlueboxMessage.State.ANY, 0, 5, BlueboxMessage.RECEIVED, true, Locale.getDefault());
+		assertEquals("Missing mail",COUNT,list.size());
+		log.info("<<<<<>>>>>"+list.size());
 		for (LiteMessage jo : list) {
 			String url = "/"+JSONMessageUtilHandler.JSON_ROOT+"/"+jo.getIdentifier()+"/"+JSONMessageUtilHandler.LINKS;
 			JSONObject js = getRestJSON(url);
@@ -27,7 +45,9 @@ public class TestJSONMessageUtilHandler extends BaseServletTest {
 				assertNotNull("Link text not found",ja.getJSONObject(i).getString("text"));
 				assertNotNull("Link href not found",ja.getJSONObject(i).getString("href"));
 			}
+			log.info("<<<<<>>>>>");
 			log.info(js.toString(3));
+			log.info("<<<<<>>>>>");
 		}
 	}
 
