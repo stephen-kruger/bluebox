f4479fc950f932a00358ba717ba1a183a1003cce
Stephen Kruger <stephen.kruger@yahoo.com>
2017-03-25 18:15:37 +0000

    Fix readme markup

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-#Bluebox
+# Bluebox
 Many teams develop software that rely heavily on sending emails (for sign-up, social software notifications etc). Properly testing this software requires multiple "fake" email addesses to allow testing of the capabilities of the software from various points of view and roles Traditionally we either used Google Mail (which allowed random prefixes to be added to email addresses), or other services such as Mailinator However sending such emails from unreleased products, potentially containing trade secrets, non-public product information and other private information to the public internet is not feasible, and an internal service was needed.
 
 Bluebox is a fully functional promiscuous, non-forwarding SMTP server and mailbox web interface to allow receiving of arbitrary emails for use in testing and development environments. Inspired by the very useful Mailinator service. Provides API for scripted use by test automation.
 
-#Installing
-##Installing in Websphere Liberty
+# Installing
+## Installing in Websphere Liberty
 Download and install the latest Liberty profile from https://developer.ibm.com/assets/wasdev/
 
 Create a Liberty profile :
@@ -59,16 +59,16 @@ Download the war file from here :https://github.com/stephen-kruger/bluebox/relea
 
 You can then start the server by running the command <wlp_root>/wlp/bin/server start bluebox
 
-##Database options
+## Database options
 On startup, if Bluebox detects a running instance of MongoDB, it will automatically set up and use that. Otherwise it will use an embedded H2 database which is a little less performant than MongoDB.
 
-##Check the SMTP server
+## Check the SMTP server
 For security reasons the SMTP server will start on port 2500 instead of the expected port 25. So to recieve emails you need to either map port 25 to 2500, or change the config (see Additional Customisation section below)
 
-###Option A: Map the port
+### Option A: Map the port
 On Linux run this command
 ```iptables -t nat -A PREROUTING -i eth0 -p tcp -m tcp --dport 25 -j REDIRECT --to-ports 2500```
-###Option B: Change the config
+### Option B: Change the config
 On Windows systems, or if you don't mind starting the application server as root, you can change the default settings overriding them in bluebox.properties This file will be checked for in the Websphere Liberty server directory (e.g. [wlp dir]/usr/server/defaultServer/bluebox.properties).
 Some commonly used properties are :
 ```properties
@@ -86,30 +86,30 @@ bluebox_message_max=160000
 bluebox_daemon_delay=60
 ```
 
-###Verify the smtp server is working
+### Verify the smtp server is working
 You can test it using simple telnet to see if the SMTP server responds on the default SMTP port:
 ```
 telnet myhost.name 25
 220 myhost.name ESMTP BlueBox V4.5.2
 ```
 
-#FAQ
-##How do I use it?
+# FAQ
+## How do I use it?
 The easiest way : simply send an email to any address you like using the @bluebox.xxx.com domain For example send a test mail using your Notes client to test@bluebox.xxx.com or fake.mail@bluebox.xxx.com Then enter the email address into the form here at BlueBox? to show your inbox Another way : set your SMTP server to bluebox.xxx.com, you can send emails to any address and any domain (although they will all be delivered and available only on bluebox.xxx.com)
 
-##How long will my email be available?
+## How long will my email be available?
 Messages will be automatically deleted after 48 hours Mails in the Trash folder (i.e. explicitly deleted by the user)will be removed after 12 hours If you require it for longer, simply use the Download button to save a copy to disk, or attach to a defect report You can then use the Upload functionality to re-inject it into the system as many times as you like
 
-##SMTP Open Relay Test scans are complaining Bluebox is an open relay
+## SMTP Open Relay Test scans are complaining Bluebox is an open relay
 These scans generally test if an SMTP server accepts email from arbitrary domains. By design, Bluebox IS very forgiving, and will accept any messages unless otherwise configured via blacklist or whitelist However no mails, regardless of recipient, are EVER forwarded on to other systems. So any scan which implies Bluebox is an open relay is not correctly implemented, as they would need to validate that a test email is actually delivered to a different MX domain - but generally these tests are lazily written, and only go as far as testing that the SMTP server performs the "accept" handshake. It is possible to defend against some of these poorly written relay scans by intelligently configuring the bluebox_to_blacklist or bluebox_from_blacklist in bluebox.properties: Run the offending scan program, and check the app server logs to detect what email is being used to run the test (e.g. test@microsoft.com), and then add that domain to the bluebox_to_blacklist, which will cause rejection of any messages from that domain, hopefully fooling the simple-minded Open Relay scan.
 
-##Can I send mails to other domains?
+## Can I send mails to other domains?
 BlueBox? itself does not do any mail forwarding, only incoming mail is accepted It's not very scrupulous about destination addresses, so if you have set bluebox as your smtp server, you can send to any hostnames that you like. Such as stephen@yahoo.com, steve@xxx.com, or any other domain you might want
 
-#Terms and conditions of usage
+# Terms and conditions of usage
 Please do not use BlueBox for any business critical applications Please respect others may be using the system - don't delete mails which are not yours This software is provided as is. Availability and support is limited to the goodwill of the author, or patches you may wish to submit
 
-#License
+# License
 Copyright 2016 Stephen Kruger
 
 Licensed under the Apache License, Version 2.0 (the "License");
--- src/main/java/com/bluebox/smtp/BlueboxMessageHandlerFactory.java
+++ src/main/java/com/bluebox/smtp/BlueboxMessageHandlerFactory.java
@@ -19,115 +19,112 @@ import com.bluebox.smtp.storage.StorageIf;
 
 
 public class BlueboxMessageHandlerFactory extends SimpleMessageListenerAdapter implements MessageHandlerFactory {
-	private static final Logger log = LoggerFactory.getLogger(BlueboxMessageHandlerFactory.class);
+    private static final Logger log = LoggerFactory.getLogger(BlueboxMessageHandlerFactory.class);
 
-	private List<String> smtpBlackList;
-	
-	public BlueboxMessageHandlerFactory(Inbox inbox) {
-		super(inbox);
-		Config config = Config.getInstance();
-		StorageIf si;
-//		do {
-			si = StorageFactory.getInstance();
-//		} while (si==null);
-		setSMTPBlacklist(Config.toList(si.getProperty(Config.BLUEBOX_SMTPBLACKLIST, config.getString(Config.BLUEBOX_SMTPBLACKLIST))));
-		inbox.setBlueboxMessageHandlerFactory(this);
-	}
-	
-	public void setSMTPBlacklist(String props) {
-		setSMTPBlacklist(Config.toList(props));
-	}
-	
-	public void setSMTPBlacklist(List<String> list) {
-		Config.getInstance().setStringList(Config.BLUEBOX_SMTPBLACKLIST, list);
-		
-		// persist to storage
-		StorageFactory.getInstance().setProperty(Config.BLUEBOX_SMTPBLACKLIST,Config.toString(Config.getInstance().getStringList(Config.BLUEBOX_SMTPBLACKLIST)));
-		
-		smtpBlackList = list;
-	}
+    private List<String> smtpBlackList;
 
-	public void addSMTPBlackList(String domain) {
-		if (domain!=null) {
-			if (domain.trim().length()>0) {
-				log.info("Blacklisting smtp server {}",domain);
-				smtpBlackList.remove(domain);
-				smtpBlackList.add(domain);
-				setSMTPBlacklist(smtpBlackList);
-			}
-		}
-	}
+    public BlueboxMessageHandlerFactory(Inbox inbox) {
+	super(inbox);
+	Config config = Config.getInstance();
+	StorageIf si;
+	si = StorageFactory.getInstance();
+	setSMTPBlacklist(Config.toList(si.getProperty(Config.BLUEBOX_SMTPBLACKLIST, config.getString(Config.BLUEBOX_SMTPBLACKLIST))));
+	inbox.setBlueboxMessageHandlerFactory(this);
+    }
 
-	public void removeSMTPBlackList(String domain) {
-		if (domain!=null) {
-			if (domain.trim().length()>0) {
-				log.info("Un-blacklisting smtp server {}",domain);
-				smtpBlackList.remove(domain);
-				setSMTPBlacklist(smtpBlackList);
-			}
-		}
-	}
+    public void setSMTPBlacklist(String props) {
+	setSMTPBlacklist(Config.toList(props));
+    }
 
-	public boolean isBlackListed(MessageContext ctx) {
-		return isBlackListed(ctx.getHelo(),ctx.getRemoteAddress().toString());
+    public void setSMTPBlacklist(List<String> list) {
+	Config.getInstance().setStringList(Config.BLUEBOX_SMTPBLACKLIST, list);
+
+	// persist to storage
+	StorageFactory.getInstance().setProperty(Config.BLUEBOX_SMTPBLACKLIST,Config.toString(Config.getInstance().getStringList(Config.BLUEBOX_SMTPBLACKLIST)));
+
+	smtpBlackList = list;
+    }
+
+    public void addSMTPBlackList(String domain) {
+	if (domain!=null) {
+	    if (domain.trim().length()>0) {
+		log.info("Blacklisting smtp server {}",domain);
+		smtpBlackList.remove(domain);
+		smtpBlackList.add(domain);
+		setSMTPBlacklist(smtpBlackList);
+	    }
 	}
-	
-	public boolean isBlackListed(String helo, String address) {
-		try {
-			for (String bdomain : smtpBlackList) {
-				if ((helo.contains(bdomain))||(address.contains(bdomain))) {
-					return true;
-				}
-			}
+    }
+
+    public void removeSMTPBlackList(String domain) {
+	if (domain!=null) {
+	    if (domain.trim().length()>0) {
+		log.info("Un-blacklisting smtp server {}",domain);
+		smtpBlackList.remove(domain);
+		setSMTPBlacklist(smtpBlackList);
+	    }
+	}
+    }
+
+    public boolean isBlackListed(MessageContext ctx) {
+	return isBlackListed(ctx.getHelo(),ctx.getRemoteAddress().toString());
+    }
+
+    public boolean isBlackListed(String helo, String address) {
+	try {
+	    for (String bdomain : smtpBlackList) {
+		if ((helo.contains(bdomain))||(address.contains(bdomain))) {
+		    return true;
 		}
-		catch (Throwable t) {
-			//t.printStackTrace();
-		    log.error("Receieved null helo ({}) or address ({})",helo,address);
-		}
-		return false;
+	    }
+	}
+	catch (Throwable t) {
+	    log.error("Receieved null helo ({}) or address ({})",helo,address);
+	}
+	return false;
+    }
+
+    @Override
+    public MessageHandler create(MessageContext ctx) {
+	if (isBlackListed(ctx)) {
+	    log.warn("Rejecting spam message from {} with address {}",ctx.getHelo(),ctx.getRemoteAddress().toString());
+	    return new SpamMessageHandler(ctx);
+	}
+	log.debug("Accepting message from {} with address {}",ctx.getHelo(),ctx.getRemoteAddress().toString());
+	return super.create(ctx);
+    }
+
+    public class SpamMessageHandler implements MessageHandler {
+
+	private String from, recipient;
+	private MessageContext ctx;
+
+	public SpamMessageHandler(MessageContext ctx) {
+	    this.ctx = ctx;
 	}
 
 	@Override
-	public MessageHandler create(MessageContext ctx) {
-		if (isBlackListed(ctx)) {
-			log.warn("Rejecting spam message from {} with address {}",ctx.getHelo(),ctx.getRemoteAddress().toString());
-			return new SpamMessageHandler(ctx);
-		}
-		log.debug("Accepting message from {} with address {}",ctx.getHelo(),ctx.getRemoteAddress().toString());
-		return super.create(ctx);
+	public void from(String from) throws RejectException {
+	    this.from = from;
+	    throw new RejectException("From not allowed ("+from+")");			
 	}
 
-	public class SpamMessageHandler implements MessageHandler {
-
-		private String from, recipient;
-		private MessageContext ctx;
-
-		public SpamMessageHandler(MessageContext ctx) {
-			this.ctx = ctx;
-		}
-
-		@Override
-		public void from(String from) throws RejectException {
-			this.from = from;
-			throw new RejectException("From not allowed ("+from+")");			
-		}
-
-		@Override
-		public void recipient(String recipient) throws RejectException {
-			this.recipient = recipient;
-			throw new RejectException("Recipient not allowed ("+recipient+")");						
-		}
-
-		@Override
-		public void data(InputStream data) throws RejectException, TooMuchDataException, IOException {
-			data.close();
-			throw new RejectException("Data not allowed");						
-		}
-
-		@Override
-		public void done() {
-			log.warn("Rejecting spam to {} from {} based on blacklisted SMTP server {} {}",recipient,from,ctx.getHelo(),ctx.getRemoteAddress());
-		}
-
+	@Override
+	public void recipient(String recipient) throws RejectException {
+	    this.recipient = recipient;
+	    throw new RejectException("Recipient not allowed ("+recipient+")");						
 	}
+
+	@Override
+	public void data(InputStream data) throws RejectException, TooMuchDataException, IOException {
+	    data.close();
+	    throw new RejectException("Data not allowed");						
+	}
+
+	@Override
+	public void done() {
+	    log.warn("Rejecting spam to {} from {} based on blacklisted SMTP server {} {}",recipient,from,ctx.getHelo(),ctx.getRemoteAddress());
+	}
+
+    }
 }
